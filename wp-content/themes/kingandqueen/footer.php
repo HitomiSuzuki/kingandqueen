f9cf7097c1cc4aa278c4f2907f0c744dba096d9b
HitomiSUZUKI <it.is.a.fishstory@gmail.com>
2024-12-05 17:53:13 +0900

    add footer file

---
+++ wp-content/themes/kingandqueen/footer.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+
+    <div class="footer__logo"><img src="<?php echo get_template_directory_uri() ?>/images/logo.svg" alt="ロゴ"></div>
+    <div class="footer__inner">
+        <a href="<?php echo home_url(); ?>/concept" class="footer__item">コンセプト</a>
+        <a href="<?php echo home_url(); ?>/bath" class="footer__item">サウナ・お風呂</a>
+        <a href="<?php echo home_url(); ?>/waterbath" class="footer__item">水風呂</a>
+        <a href="<?php echo home_url(); ?>/relaxation" class="footer__item">リラクゼーション</a>
+        <a href="<?php echo home_url(); ?>/stonesauna" class="footer__item">岩盤浴</a>
+        <!-- <a href="<?php echo home_url(); ?>/coming-soon" class="footer__item">屋外温浴バルコニー</a> -->
+        <a href="<?php echo home_url(); ?>/food" class="footer__item">フードコート</a>
+        <a href="<?php echo home_url(); ?>/guide" class="footer__item">利用案内</a>
+        <a href="<?php echo home_url(); ?>/recruit" class="footer__item">採用情報</a>
+        <a href="<?php echo home_url(); ?>/company" class="footer__item">会社概要</a>
+        <a href="<?php echo home_url(); ?>/access" class="footer__item">アクセス</a>
+    </div>
+    <div class="footer__bottom">
+        <p class="footer__copyright">Copyright ©キングアンドクイーン. All rights reserved.</p>
+        <p class="footer__toTop" id="page-top"></p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
