d9f754ef6cdfb4eaaee79d8e1631c851c6895dd3
HitomiSUZUKI <it.is.a.fishstory@gmail.com>
2024-12-06 16:19:12 +0900

    footer

--- wp-content/themes/kingandqueen/footer.php
+++ wp-content/themes/kingandqueen/footer.php
@@ -1,23 +1,52 @@
 <footer class="footer">
-
-    <div class="footer__logo"><img src="<?php echo get_template_directory_uri() ?>/images/logo.svg" alt="ロゴ"></div>
-    <div class="footer__inner">
-        <a href="<?php echo home_url(); ?>/concept" class="footer__item">コンセプト</a>
-        <a href="<?php echo home_url(); ?>/bath" class="footer__item">サウナ・お風呂</a>
-        <a href="<?php echo home_url(); ?>/waterbath" class="footer__item">水風呂</a>
-        <a href="<?php echo home_url(); ?>/relaxation" class="footer__item">リラクゼーション</a>
-        <a href="<?php echo home_url(); ?>/stonesauna" class="footer__item">岩盤浴</a>
-        <!-- <a href="<?php echo home_url(); ?>/coming-soon" class="footer__item">屋外温浴バルコニー</a> -->
-        <a href="<?php echo home_url(); ?>/food" class="footer__item">フードコート</a>
-        <a href="<?php echo home_url(); ?>/guide" class="footer__item">利用案内</a>
-        <a href="<?php echo home_url(); ?>/recruit" class="footer__item">採用情報</a>
-        <a href="<?php echo home_url(); ?>/company" class="footer__item">会社概要</a>
-        <a href="<?php echo home_url(); ?>/access" class="footer__item">アクセス</a>
-    </div>
-    <div class="footer__bottom">
-        <p class="footer__copyright">Copyright ©キングアンドクイーン. All rights reserved.</p>
-        <p class="footer__toTop" id="page-top"></p>
-    </div>
+    <a class="footer__logo"><img src="<?php echo get_template_directory_uri() ?>/images/footer/footerLogo.svg" alt="ロゴ"></a>
+    <ul class="footer__menues">
+        <li>
+            <a href="<?php echo home_url(); ?>/concept" class="footer__item">コンセプト</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/bath" class="footer__item">サウナ・お風呂</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/waterbath" class="footer__item">水風呂</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/stonesauna" class="footer__item">岩盤浴</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/food" class="footer__item">フードコート</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/relaxation" class="footer__item">リラクゼーション</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/guide" class="footer__item">利用案内</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/access" class="footer__item">アクセス・シャトルバス</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/contact" class="footer__item">お問い合わせ</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/company" class="footer__item">会社概要</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+        <li>
+            <a href="<?php echo home_url(); ?>/recruit" class="footer__item">採用情報</a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer/arrow03.svg" alt="">
+        </li>
+    </ul>
+    <p class="footer__copyright">Copyright ©キングアンドクイーン. All rights reserved.</p>
 </footer>
 
 <?php wp_footer(); ?>
