87432cbc95820fa3a7d0ed240e0ef94b72682669
gb-designer01 <gb.designer.one@gmail.com>
2024-12-12 16:40:28 +0900

    page.php とconcept.phpの追加

---
+++ wp-content/themes/kingandqueen/pages/concept.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: コンセプト
+Template Post Type: page
+*/
+?>
+<?php get_header(); ?>
+<div class="concept">
+	<div class="concept-header">
+		<div class="title-box">
+			<h2>コンセプト</h2>
+		</div>
+	</div>
+	
+</div>
+<?php get_template_part('/sidenav'); ?>
+<?php get_footer(); ?>
